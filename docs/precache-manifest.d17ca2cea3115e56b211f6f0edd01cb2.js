3898c5fccade0ed6474427bf38bc1295af40e95d
Jesus Milano <jesusmilano96@gmail.com>
2020-10-20 16:39:32 -0500

    docs rename

---
+++ docs/precache-manifest.d17ca2cea3115e56b211f6f0edd01cb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d83fde286f2986f21d15fff1abe036f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6ddc45cabb0e5031c08",
+    "url": "/static/css/main.f96fa256.chunk.css"
+  },
+  {
+    "revision": "eaf967ea8bbf23a58772",
+    "url": "/static/js/2.07036bef.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.07036bef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6ddc45cabb0e5031c08",
+    "url": "/static/js/main.ebe75ef9.chunk.js"
+  },
+  {
+    "revision": "c2a4463da067d7ee0a4c",
+    "url": "/static/js/runtime-main.f3c16400.js"
+  }
+]);
